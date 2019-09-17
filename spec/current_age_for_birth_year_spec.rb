f90377b527dc35e215d2dff1c98406088a355215
thuong nguyen <thuongnn6983@student-topica.edu.vn>
2019-09-17 12:59:27 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -6,7 +6,7 @@ describe "current_age_for_birth_year method" do
     def current_age_for_birth_year(birth_year)
       2003-birth_year
     end
-    age_of_person = current_age_for_birth_year(birth_year)
+    age_of_person = current_age_for_birth_year(1984)
 
     expect(age_of_person).to eq(19)
   end
